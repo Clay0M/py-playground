b3cef81b4bc7bf4fa6e07b910171a1672ddcd853
Clay0M <clayton.m.miller@gmail.com>
2022-07-17 15:34:51 -0500

    #1, #2: Renaming value to node_value for clarity, also adding type checks for left/right_node.

--- src/main/py_trees/binary_tree.py
+++ src/main/py_trees/binary_tree.py
@@ -1,12 +1,14 @@
 class BinaryTreeNode:
-    def __init__(self, value, left_node, right_node):
-        self.value = value
+    def __init__(self, node_value, left_node, right_node):
+        self.node_value = node_value
+        assert isinstance(left_node, BinaryTreeNode or type(None))
         self.left_node = left_node
+        assert isinstance(right_node, BinaryTreeNode or type(None))
         self.right_node = right_node
 
     @property
-    def get_value(self):
-        return self.value
+    def get_node_value(self):
+        return self.node_value
 
     @property
     def get_left_node(self):
