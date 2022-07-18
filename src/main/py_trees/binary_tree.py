284f0ed1b7694230c284a5198034c1aff57447f9
Clay0M <clayton.m.miller@gmail.com>
2022-07-18 11:56:11 -0500

    #1, #2: Removed redundant functions (holdovers from my Java tendencies).

--- src/main/py_trees/binary_tree.py
+++ src/main/py_trees/binary_tree.py
@@ -5,15 +5,3 @@ class BinaryTreeNode:
         self.left_node = left_node
         assert isinstance(right_node, BinaryTreeNode or type(None))
         self.right_node = right_node
-
-    @property
-    def get_node_value(self):
-        return self.node_value
-
-    @property
-    def get_left_node(self):
-        return self.left_node
-
-    @property
-    def get_right_node(self):
-        return self.right_node
