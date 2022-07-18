749d8e7a345e76cdded3f89f19bcad29d9f07c75
Clay0M <clayton.m.miller@gmail.com>
2022-07-18 12:06:21 -0500

    #1, #2: Some formatting and adding BinaryTree class.

--- src/main/py_trees/binary_tree.py
+++ src/main/py_trees/binary_tree.py
@@ -1,7 +1,15 @@
 class BinaryTreeNode:
+
     def __init__(self, node_value, left_node, right_node):
         self.node_value = node_value
         assert isinstance(left_node, BinaryTreeNode or type(None))
         self.left_node = left_node
         assert isinstance(right_node, BinaryTreeNode or type(None))
         self.right_node = right_node
+
+
+class BinaryTree:
+
+    def __init__(self, root_node):
+        assert isinstance(root_node, BinaryTreeNode)
+        self.root_node = root_node
